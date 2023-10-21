af5397084a05b3fee2ea698466f5eb592a4cf746
ulan atabekov <45645644z@gmail.com>
2023-10-21 14:07:07 +0600

    finished task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const infoUser = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    tel: '',
+  };
+
+    const [form, setForm] = useState(infoUser);
+
+    const sub = () => {
+        console.log(form);
+    };
+
+    return (
+        <>
+        <div className='app' >
+          <label htmlFor='fname'>First Name:</label>
+            <input type="text"
+                   id='fname'
+                   placeholder='Enter name'
+                    value={form.firstName}
+                    onChange={e => {setForm({...form,
+                                                                  firstName: e.target.value})}}/>
+          <label htmlFor="lname">Last Name: </label>
+            <input type="text"
+                   id='lname'
+                   placeholder='Enter last name'
+                   value={form.lastName}
+                   onChange={e => {setForm({...form,
+                       lastName: e.target.value})}}/>
+          <label htmlFor="email">Email: </label>
+            <input type="text"
+                   id='email'
+                   value={form.email}
+                   placeholder='Enter email'
+                   onChange={e => {setForm({...form,
+                       email: e.target.value})}}/>
+          <label htmlFor="tel">Telephone: </label>
+            <input type="text"
+                   id='tel'
+                   value={form.tel}
+                   placeholder='Enter telephone number'
+                   onChange={e => {setForm({...form,
+                       tel: e.target.value})}}/>
+            <div>
+            <button className='btn' onClick={() => sub()}>Submit</button>
+            </div>
+        </div>
+        </>
+    );
+};
 
 export default App;
